167ddc141881ebde0f5331444c8bcc8c0944edc2
kfrFahim <kfrfahim3@gmail.com>
2023-08-11 02:28:53 +0600

    review section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Instructor from './Instructor/Instructor';
+import Review from './Review/Review';
 
 const Home = () => {
     return (
     <section>
         <Instructor></Instructor>
+        <Review></Review>
     </section>
     );
 };
