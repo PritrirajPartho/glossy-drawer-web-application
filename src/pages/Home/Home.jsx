1394a8360b1ec6c2d56a01681cbc77a1edae9676
sakib5452 <najmus191@gmail.com>
2023-08-11 02:00:17 +0600

    courses

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Instructor from './Instructor/Instructor';
+import Course from './Course/Course';
 
 const Home = () => {
     return (
-    <section>
-        <Instructor></Instructor>
-    </section>
+        <section>
+            <Course></Course>
+            <Instructor></Instructor>
+        </section>
     );
 };
 
