5d41d1cfdb914604bc7c18d496c0232ebe6b44ab
PritrirajPartho <dapartho5616@gmail.com>
2023-08-11 01:16:56 +0600

    layout-change

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Instructor from './Instructor/Instructor';
+
+const Home = () => {
+    return (
+    <section>
+        <Instructor></Instructor>
+    </section>
+    );
+};
+
+export default Home;
