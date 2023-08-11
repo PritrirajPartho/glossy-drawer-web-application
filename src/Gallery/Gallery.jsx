27da7b1a5641162d6d146ad39b30967c72637c24
ashraful amin <ashrafulamin654@gmail.com>
2023-08-11 19:39:01 +0600

    gallery sectionn

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -10,7 +10,7 @@ const Gallery = () => {
 
     return (
         <div>
-        <h2 className="text-3xl font-bold text-green-700 text-center mt-4 mb-4">Courses Gallery</h2>
+        <h2  className="text-3xl font-bold text-green-700 text-center mt-4 mb-4">Courses Gallery</h2>
 
 
         <div className="mx-auto container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
@@ -20,8 +20,8 @@ const Gallery = () => {
                     <div key={gallery._id} className="card w-96 bg-base-100 shadow-xl  mb-4">
                         <figure><img className="rounded w-52 h-52" src={gallery.photoURL} /></figure>
                         <div className="card-body">
-                            <h2 className="card-title">Course</h2>
-                            <h2 className="card-title">instractor name: {gallery.instructor_name}</h2>
+                            <h2 className="card-title">Course</h2> 
+                            <h2 className="card-title">instractor namee: {gallery.instructor_name}</h2>
                             <h2 className="card-title">class name: {gallery.course_name}</h2>
                             <h2 className="card-title">rating: {gallery.rating}</h2>
                             <h2 className="card-title">price: {gallery.price}</h2>
