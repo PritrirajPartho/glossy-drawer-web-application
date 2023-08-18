97c018065ab1e8ea110456168a059ecc51a60a53
ashraful amin <ashrafulamin654@gmail.com>
2023-08-18 12:34:13 +0600

    gallery updated

--- src/pages/Home/Gallery/Gallery.jsx
+++ src/pages/Home/Gallery/Gallery.jsx
@@ -15,7 +15,7 @@ const Gallery = () => {
     <section className="py-6 dark:bg-gray-800">
       <div className="container flex flex-col justify-center p-4 mx-auto">
 
-        <h2 className="text-3xl font-bold text-slate-500 text-center mt-4 mb-4"> Gallery</h2>
+        <h2 className="text-3xl font-bold text-slate-500 text-center mt-4 mb-4"> Gallerys</h2>
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 sm:grid-cols-2">
 
 
