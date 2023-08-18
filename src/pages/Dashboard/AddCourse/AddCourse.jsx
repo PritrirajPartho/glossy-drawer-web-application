8a6006a88450d1176c3aed4a462342a5b531b934
Badhon sarker <badhonsarker700@gmail.com>
2023-08-18 20:15:41 +0600

    add course section customize

--- src/pages/Dashboard/AddCourse/AddCourse.jsx
+++ src/pages/Dashboard/AddCourse/AddCourse.jsx
@@ -18,10 +18,11 @@ function AddCourse() {
   };
 
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-10 bg-slate-700">
-      <h2 className="text-3xl font-bold text-center mb-9 text-white mb-2">Added Course</h2>
+    <div className="w-full bg-slate-700">
+    <div className=" px-4 sm:px-6 lg:px-8 py-10  bg-slate-700">
+      <h2 className="text-3xl font-bold ml-[170px] text-center mb-9 text-white ">Added Course</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 ml-[170px]'>
           <div className="mb-4">
             <label htmlFor="className" className="block mb-1 font-medium text-xl text-white">
               Course Name
@@ -147,15 +148,16 @@ function AddCourse() {
           
         </div>
 
-        <div className="flex justify-end">
+        <div className="flex justify-center">
           <button
             type="submit"
-            className="w-full text-xl bg-blue-950 hover:bg-blue-900 text-white font-medium px-4 py-2 rounded-md"
+            className=" w-full ml-[170px] text-xl bg-blue-950 hover:bg-blue-900 text-white font-medium px-4 py-2 rounded-md"
           >
             Add Course
           </button>
         </div>
       </form>
+    </div>
     </div>
   );
 }
