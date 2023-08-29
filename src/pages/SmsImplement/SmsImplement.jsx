5c51d4a13a6ac76bfe71b2e06a5a4775a7bbffb2
ashraful amin <ashrafulamin654@gmail.com>
2023-08-29 17:17:36 +0600

    Sms implement by deva

---
+++ src/pages/SmsImplement/SmsImplement.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
+const SmsImplement = () => {
+    return (
+        <div>
+            <MessengerCustomerChat
+                pageId="108710062332316"
+                appId="281843884606103"
+            />
+        </div>
+    );
+};
+
+export default SmsImplement;
