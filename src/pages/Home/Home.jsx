d08b49ee0faec0a344621a0ce4a990349ab7d352
Badhon sarker <badhonsarker700@gmail.com>
2023-08-31 02:42:21 +0600

    merge conflit resolved

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,33 +1,27 @@
 import React from 'react';
 import Instructor from './Instructor/Instructor';
-<<<<<<< HEAD
 import Review from './Review/Review';
 import Slider from './Slider/Slider'
-import Course from './Course/Course';
 import Gallery from './Gallery/Gallery';
+import Course from './Course/Course';
 
-=======
 import Course from './Course/Course';
 import Slider from './Slider/Slider';
 import Contacts from '../Contacts/Contacts';
 import Review from './Review/Review';
->>>>>>> 292350063d335ef8cb764a1ee196a6e126a6aed9
 
 const Home = () => {
     return (
         <section>
             <Slider></Slider>
             <Course></Course>
-<<<<<<< HEAD
             <Instructor></Instructor>
             <Review></Review>
             <Gallery></Gallery>
-=======
             <FinalGallery></FinalGallery>
             {/* <Gallery></Gallery> */}
             <Instructor></Instructor>
             <Contacts></Contacts>
->>>>>>> 292350063d335ef8cb764a1ee196a6e126a6aed9
         </section>
     );
 };
