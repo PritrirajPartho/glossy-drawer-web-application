cc59c77d452e63c087925349b16aaf629ad7a4c2
ashraful amin <ashrafulamin654@gmail.com>
2023-09-13 23:29:00 +0600

    updated for marge

--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FAQ = () => {
     return (
-<section className="dark:bg-gray-800 dark:text-gray-100">
+<section className="dark:bg-gray-700 dark:text-gray-100">
 	<div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
 		<h2 className="text-2xl text-center mb-4 font-semibold sm:text-4xl">Frequently Asked Questions</h2>
 		<div className="space-y-4">
