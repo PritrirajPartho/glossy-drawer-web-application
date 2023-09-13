69d4de37c2ec100db371df8a6d077c3a18cecf1a
kfrFahim <kfrfahim3@gmail.com>
2023-09-14 02:38:32 +0600

    canvas update

--- src/pages/Canvas/DrawingCanvas.jsx
+++ src/pages/Canvas/DrawingCanvas.jsx
@@ -3,7 +3,6 @@ import "./DrawingCanvas.css";
 import { FaEraser, FaPaintBrush, FaDownload } from "react-icons/fa";
 import { GrClear, GrUndo, GrRedo } from "react-icons/gr";
 
-
 const DrawingCanvas = () => {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
@@ -13,6 +12,7 @@ const DrawingCanvas = () => {
   const [brushWidth, setBrushWidth] = useState(5);
   const [canvasStates, setCanvasStates] = useState([]);
   const [currentStateIndex, setCurrentStateIndex] = useState(-1);
+  const [activeButton, setActiveButton] = useState("draw");
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -126,63 +126,62 @@ const DrawingCanvas = () => {
 
   return (
     <div className="w-full h-[700px]">
-
       <div className="flex justify-center gap-2 h-[700px] bg-[#555] ">
         <div className="flex flex-col h-[500px] p-3 my-5 mt-6 bg-slate-800  ">
-         <div className="grid grid-cols md:grid-cols-2 gap-5 p-4 ">
-         <button
-  title="Draw"
-  className={`btn btn-outline mx-auto cursor-pointer border-2 hover:w-12 hover:duration-1000 ${
-    activeButton === "draw" ? "active-button" : ""
-  }`}
-  onClick={() => {
-    setToDraw();
-    setActiveButton("draw");
-  }}
->
-  <FaPaintBrush></FaPaintBrush>
-</button>
-<button
-  title="Erase"
-  className={`btn btn-outline mx-auto cursor-pointer border-2 hover:w-12 hover:duration-1000 ${
-    activeButton === "erase" ? "active-button" : ""
-  }`}
-  onClick={() => {
-    setToErase();
-    setActiveButton("erase");
-  }}
->
-  <FaEraser></FaEraser>
-</button>
-          <input
-            type="color"
-            value={currentColor}
-            onChange={(e) => setCurrentColor(e.target.value)}
-            className="w-[50px] h-[50px] mx-auto btn-outline border-2 border-green-400 hover:border-2 hover:border-green-700 hover:duration-1000 "
-            title="Pick a Color"
-          />
-          <button
-            title="Clear Canvas"
-            className="btn btn-outline btn-error text-gray mx-auto cursor-pointer border-2  hover:w-12 hover:duration-1000"
-            onClick={clearCanvas}
-          >
-            <GrClear></GrClear>
-          </button>
-          <button
-          title="Undo"
-          className="btn btn-outline btn-accent mx-auto cursor-pointer border-2 hover:w-12 hover:duration-1000"
-          onClick={undo}
-        >
-          <GrUndo></GrUndo>
-        </button>
-        <button
-          title="Redo"
-          className="btn btn-outline btn-accent mx-auto cursor-pointer border-2 hover:w-12 hover:duration-1000"
-          onClick={redo}
-        >
-          <GrRedo></GrRedo>
-        </button>
-         </div>
+          <div className="grid grid-cols md:grid-cols-2 gap-5 p-4 ">
+            <button
+              title="Draw"
+              className={`btn btn-outline mx-auto cursor-pointer border-2 hover:w-12 hover:duration-1000 ${
+                activeButton === "draw" ? "active-button" : ""
+              }`}
+              onClick={() => {
+                setToDraw();
+                setActiveButton("draw");
+              }}
+            >
+              <FaPaintBrush></FaPaintBrush>
+            </button>
+            <button
+              title="Erase"
+              className={`btn btn-outline mx-auto cursor-pointer border-2 hover:w-12 hover:duration-1000 ${
+                activeButton === "erase" ? "active-button" : ""
+              }`}
+              onClick={() => {
+                setToErase();
+                setActiveButton("erase");
+              }}
+            >
+              <FaEraser></FaEraser>
+            </button>
+            <input
+              type="color"
+              value={currentColor}
+              onChange={(e) => setCurrentColor(e.target.value)}
+              className="w-[50px] h-[50px] mx-auto btn-outline border-2 border-green-400 hover:border-2 hover:border-green-700 hover:duration-1000 "
+              title="Pick a Color"
+            />
+            <button
+              title="Clear Canvas"
+              className="btn btn-outline btn-error text-gray mx-auto cursor-pointer border-2  hover:w-12 hover:duration-1000"
+              onClick={clearCanvas}
+            >
+              <GrClear></GrClear>
+            </button>
+            <button
+              title="Undo"
+              className="btn btn-outline btn-accent mx-auto cursor-pointer border-2 hover:w-12 hover:duration-1000"
+              onClick={undo}
+            >
+              <GrUndo></GrUndo>
+            </button>
+            <button
+              title="Redo"
+              className="btn btn-outline btn-accent mx-auto cursor-pointer border-2 hover:w-12 hover:duration-1000"
+              onClick={redo}
+            >
+              <GrRedo></GrRedo>
+            </button>
+          </div>
 
           <div className="brush-width p-1 my-2 btn-outline rounded-full mx-auto hover:btn-info hover:py-2 hover:duration-500">
             <input
